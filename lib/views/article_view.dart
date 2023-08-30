239f7c22b6759d9a8f936c68146edc017ff51508
Berkan Yükselgün <berkanyukselgun@gmail.com>
2023-08-31 02:58:12 +0300

    Article view update

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -1,27 +1,41 @@
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-class ArticleView extends StatefulWidget {
- 
- final String blogUrl;
- ArticleView({required this.blogUrl})
 
+class ArticleView extends StatefulWidget {
+  final String blogUrl;
+  ArticleView({required this.blogUrl});
 
   @override
   State<ArticleView> createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
-  final Completer<WebViewController> _completer = Completer<WebViewController>();
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: WebView(
-        initialUrl:widget.blogUrl,
-        onWebViewCreated: ((WebViewController webViewController){
-          _completer.complete(webViewController);
-        })
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Your", style: TextStyle(color: Colors.black)),
+            Text("News", style: TextStyle(color: Colors.blue))
+          ],
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: ((WebViewController webViewController) {
+            _completer.complete(webViewController);
+          }),
+        ),
       ),
     );
   }
